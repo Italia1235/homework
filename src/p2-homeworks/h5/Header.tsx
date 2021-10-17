c8e7116abed0666e117e82baccdd604482d23181
Italia1235 <1437dada@mail.ru>
2021-10-17 10:02:02 +0600

    2

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import { NavLink, Route } from 'react-router-dom'
+import s from './Header.module.css'
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    return (
+
+
+        <div className={s.navbar}>
+    <NavLink className={`${s.navlink} ${s.active}`} to={"/pre-junior"}>  <span>Prejunior</span> </NavLink>
+            <NavLink className={`${s.navlink} ${s.active}`} to={"/junior"}>  <span>Junior</span> </NavLink>
+            <NavLink className={`${s.navlink} ${s.active}`} to={"/junior+"}>  <span>Junior+</span> </NavLink>
         </div>
     )
 }
